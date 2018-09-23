249901e57d23afe2900297a2831d820ad4adfc70
thariharan <41740507+thariharan@users.noreply.github.com>
2018-09-23 21:06:35 +0530

    Create looping with numbers.sh

---
+++ looping with numbers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# To display a natural numbers using loops
+
+for i in {1..50};
+do
+	echo $i
+done
+
+
+# using for loop
+#!/bin/bash
+
+for ((i=1;i<=50; i++));
+do
+    echo $i
+done
